13b96544f34681597fe227e7dd41f37ea49d3acc
binhvu2005 <vubinh2005mt@gmail.com>
2026-05-27 00:49:56 +0700

    feat: setup S3 R2 service client and configuration in NestJS backend

--- code/backend/src/prisma/prisma.module.ts
+++ code/backend/src/prisma/prisma.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { R2Service } from './r2.service';
 
 @Global()
 @Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, R2Service],
+  exports: [PrismaService, R2Service],
 })
 export class PrismaModule {}
